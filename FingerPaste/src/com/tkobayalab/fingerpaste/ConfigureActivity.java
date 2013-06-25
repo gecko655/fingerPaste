d3d3a38c5bec37cb90d8b10998992fb194d9d978
yamamori <yamamori@sa.cs.titech.ac.jp>
2013-06-25 20:47:14 +0900

    Creating Configure Activity

--- FingerPaste/src/com/tkobayalab/fingerpaste/ConfigureActivity.java
+++ FingerPaste/src/com/tkobayalab/fingerpaste/ConfigureActivity.java
@@ -1,15 +1,35 @@
 package com.tkobayalab.fingerpaste;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
 
-public class ConfigureActivity extends Activity {
+public class ConfigureActivity extends Activity{
+	
+	private static final String PREF_KEY = "FingerPaste";
 
+	private SharedPreferences pref;
+	private SharedPreferences.Editor editor;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_configure);
+		//setContentView(R.layout.activity_configure);
+		
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PrefsFragment()).commit();	
+		pref = getSharedPreferences(PREF_KEY, Activity.MODE_PRIVATE);
+	}
+	
+	public static class PrefsFragment extends PreferenceFragment{
+		@Override
+		public void onCreate(Bundle savedInstanceState){
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.layout.activity_configure);
+		}
 	}
 
 	@Override
